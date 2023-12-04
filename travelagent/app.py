3ed9f7f769e67b8b9b173af312adebd3665a3330
nelson <gonzabato@outlook.com>
2023-12-03 19:08:46 -0500

    streamlit init

--- travelagent/app.py
+++ travelagent/app.py
@@ -2,7 +2,12 @@ import google.generativeai as palm
 import os 
 from shiny import ui, render, App, reactive, req 
 from shinyswatch import theme_picker_ui, theme_picker_server 
-from subprocess import Popen, call 
+from IPython.display import Audio, display 
+import torchaudio
+from speechbrain.pretrained import HIFIGAN, Tacotron2 
+from shinywidgets import output_widget, register_widget
+from ipywidgets import Play 
+
 
 # TODO: UI styling, better error handling to show some form of notification to the user 
 app_ui = ui.page_fluid(
@@ -12,7 +17,9 @@ app_ui = ui.page_fluid(
 
     ui.input_text_area("user_prompt", "", placeholder="Type here", autoresize = True),
     ui.input_action_button("ok_go", "Run"), 
-    ui.input_action_button("stop", "Stop Speech")
+    # ui.input_action_button("stop", "Stop Speech"),
+    output_widget("audio_player")
+    # ui.output_ui("audio_player")
 
 
     ), 
@@ -26,6 +33,8 @@ app_ui = ui.page_fluid(
 
 def server(input, output,session):
     theme_picker_server()
+    hifi_gan = HIFIGAN.from_hparams(source="speechbrain/tts-hifigan-ljspeech", savedir="tmpdir_vocoder")
+    tacotron2 = Tacotron2.from_hparams(source="speechbrain/tts-tacotron2-ljspeech", savedir="tmpdir_tts")
     try: 
         # TODO: Disable this welcome message from the API
         # TODO: Restrict the range of topics supported 
@@ -48,15 +57,20 @@ def server(input, output,session):
         with ui.Progress(min = 1, max = 20) as p:
             p.set(message="Thinking", detail="This may take a while, please wait")
             out = result()
-        return out 
+        return out
+
+    # @reactive.event(result())
     def play_audio(response):
     # TODO: Change the kind of command used to say words depending on the OS 
     # TODO: Experiment with pre-trained GANs eg the nemo toolkit 
     # TODO: Attempt to use some thing that does not depend on using the `say` command 
-        try:
-            proc = Popen(["say", response, "&"])
-        except KeyboardInterrupt:
-            proc.terminate()
+        output_, _, _ = tacotron2.encode_text(response)
+        # Running Vocoder (spectrogram-to-waveform)
+        waveforms = hifi_gan.decode_batch(output_)
+        # proc = Popen(["say", response, "&"])
+        return display(Audio(waveforms.numpy()[0], rate = 30000))
+    # register_widget("audio_player", play_audio)
+   
 
     @reactive.event(input.stop)
     def _():
@@ -69,13 +83,14 @@ def server(input, output,session):
     @reactive.event(input.ok_go)
     def _():
         ui.insert_ui(
-                  ui.tags.audio(
+                  ui.tags.html(
       
-                src = req(play_audio(result()))
+            #     src = req(play_audio(result()))
 
-            ), 
-            selector="speech",
-            where = "afterEnd",
-      
+            # ), 
+            # selector="speech",
+            # where = "afterEnd",
+         req(play_audio(result()))      
+        )
         )
 app = App(app_ui, server)
